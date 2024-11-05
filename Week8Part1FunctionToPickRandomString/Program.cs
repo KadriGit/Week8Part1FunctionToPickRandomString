e993e5a3098e376a1014dafa201d7878b63b4303
KadriGit <kadri.viira@tktk.ee>
2024-11-05 14:28:34 +0200

    Final code

--- Week8Part1FunctionToPickRandomString/Program.cs
+++ Week8Part1FunctionToPickRandomString/Program.cs
@@ -2,16 +2,14 @@
 string[] food = { "sushi", "pizza", "Chinese takeaway", "burger and chips", "chicken wings", "chips", "popcorn" };
 string[] drinks = { "mineral water", "Coca-Cola", "apple juice", "milk", "beer" };
 
-Random rnd = new Random();
+PickRandomFromArray(tvShows);
+PickRandomFromArray(food);
+PickRandomFromArray(drinks);
 
-int randomIndex = rnd.Next(0,tvShows.Length);
-string randomMovie = tvShows[randomIndex];
-Console.WriteLine(randomMovie);
-
-randomIndex = rnd.Next(0,food.Length);
-string randomFood = food[randomIndex];
-Console.WriteLine(randomFood);
-
-randomIndex = rnd.Next(0, drinks.Length);
-string randomDrinks = drinks[randomIndex];
-Console.WriteLine(randomDrinks);
+static void PickRandomFromArray(string[] someArray)
+{
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0,someArray.Length);
+    string randomeElement = someArray[randomIndex];
+    Console.WriteLine(randomeElement);
+}
