d2c46df7bda4be38f82223e2e8569b50caf63dac
KadriGit <kadri.viira@tktk.ee>
2024-11-05 14:14:06 +0200

    first

--- Week8Part1FunctionToPickRandomString/Program.cs
+++ Week8Part1FunctionToPickRandomString/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] tvShows = { "Firefly", "Office", "The Last of Us", "Friends", "Twin Peaks", "House", "Stranger Things" };
+string[] food = { "sushi", "pizza", "Chinese takeaway", "burger and chips", "chicken wings", "chips", "popcorn" };
+string[] drinks = { "mineral water", "Coca-Cola", "apple juice", "milk", "beer" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0,tvShows.Length);
+
+Console.WriteLine(randomIndex);
